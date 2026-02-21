d6a7cae586ebd4afa7bcb11b280c3b6084533574
Gronsak <4255265+Gronsak@users.noreply.github.com>
2026-02-21 22:40:27 +0100

    Change Lists to IEnumerable with backing fields and implement add, update and remove methods for each where appropriate to make sure they can't be changed externaly without calling RecalculateLedger.

--- Models/Ledger.cs
+++ Models/Ledger.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Coffer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 
 namespace Coffer.Models;
 public class Ledger
@@ -10,7 +12,7 @@ public class Ledger
         this.Name = name;
         this.Description = description;
         this.Owner = owner;
-        this.Members.Add(owner);
+        this._members.Add(owner);
         this.DefaultShareType = defaultType;
         this.DefaultCurrency = defaulCurrency;
     }
@@ -22,11 +24,31 @@ public class Ledger
     public AppUser Owner { get; set; } = new();
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime LastUpdated { get; set; } = DateTime.Now;
-    public List<Share> Shares { get; set; } = [];
-    public List<IOU> IOUs { get; set; } = [];
-    public List<AppUser> Members { get; set; } = [];
-    public List<Cost> Costs { get; set; } = [];
-    public List<Stake> Stakes { get; set; } = [];
+    [BackingField(nameof(_shares))]
+    public IEnumerable<Share> Shares
+    {
+        get { return _shares.AsEnumerable(); } 
+    }
+    [BackingField(nameof(_ious))]
+    public IEnumerable<IOU> IOUs
+    {
+        get { return _ious.AsEnumerable(); } 
+    }
+    [BackingField(nameof(_members))]
+    public IEnumerable<AppUser> Members
+    {
+        get { return _members.AsEnumerable(); } 
+    }
+    [BackingField(nameof(_costs))]
+    public IEnumerable<Cost> Costs
+    {
+        get { return _costs.AsEnumerable(); }
+    }
+    [BackingField(nameof(_stakes))]
+    public IEnumerable<Stake> Stakes
+    {
+        get { return _stakes.AsEnumerable(); }
+    }
     public bool Active { get; set; } = true;
     public ShareType DefaultShareType { get; set; } = ShareType.Shares;
     public Currency DefaultCurrency { get; set; } = new();
@@ -44,17 +66,22 @@ public class Ledger
                 where iou.Settled == false
                 select iou.Settled).FirstOrDefault(true);
     }
+    private List<Share> _shares = [];
+    private List<IOU> _ious = [];
+    private List<AppUser> _members = [];
+    private List<Cost> _costs = [];
+    private List<Stake> _stakes = [];
 
-    public bool CalculateShares()
+    private void CalculateShares()
     {
         throw new NotImplementedException();
     }
-    public bool CalculateStakes()
+    private void CalculateStakes()
     {
         var stakeHolders = (from cost in Costs
                             select cost.PayedBy).Distinct();
         if(stakeHolders is null)
-            return false;
+            return;
 
         foreach (var holder in stakeHolders)
         {
@@ -71,53 +98,109 @@ public class Ledger
             }
             else
             {
-                Stakes.Add(new(holder, amount));
+                _stakes.Add(new(holder, amount));
             }
         }
-        return true;
     }
-    public bool CalculateIOUs()
+    private void CalculateIOUs()
     {
         throw new NotImplementedException();
+    }
+    private void RecalculateLedger()
+    {
+        CalculateStakes();
+        // CalculateShares();
+        // CalculateIOUs();
     }
     public bool AddCost(Cost cost)
     {
-        if(!Active||Costs.Contains(cost))
+        if(!Active||_costs.Contains(cost))
             return false;
 
-        Costs.Add(cost);
-        CalculateStakes();
+        _costs.Add(cost);
+        RecalculateLedger();
         return true;
     }
-    public bool AddShare(Share share)
+    public bool UpdateCost(Cost cost)
     {
-        if(!Active)
+        if(!Active||!_costs.Any(c => c.Id == cost.Id))
             return false;
-        throw new NotImplementedException();
-    }
-    public bool AddUser(AppUser user)
-    {
-        if(!Active)
-            return false;
-        throw new NotImplementedException();
+        
+        var existingCost = _costs.Single(c => c.Id == cost.Id);
+        existingCost.UpdateCost(cost);
+        RecalculateLedger();
+
+        return true;
     }
     public bool RemoveCost(Cost cost)
     {
-        if(!Active)
+        if(!Active||!_costs.Any(c => c.Id == cost.Id))
             return false;
-        throw new NotImplementedException();
+        
+        var success = _costs.Remove(cost);
+        if (success)
+        {
+            RecalculateLedger();
+            return success;
+        }
+        else
+            return success;
+    }
+    public bool AddShare(Share share)
+    {
+        if(!Active||_shares.Any(s => s.Id == share.Id))
+            return false;
+        _shares.Add(share);
+        RecalculateLedger();
+        return true;
+    }
+    public bool UpdateShare(Share share)
+    {
+        if(!Active||!_shares.Any(s => s.Id == share.Id))
+            return false;
+        
+        var existingShare = _shares.Single(s => s.Id == share.Id);
+        existingShare.UpdateShare(share);
+        RecalculateLedger();
+
+        return true;
     }
     public bool RemoveShare(Share share)
     {
-        if(!Active)
+        if(!Active||!_shares.Any(s => s.Id == share.Id))
             return false;
-        throw new NotImplementedException();
+        
+        var success = _shares.Remove(share);
+        if (success)
+        {
+            RecalculateLedger();
+            return success;
+        }
+        else
+            return success;
     }
-    public bool RemoveUser(AppUser user)
+    public bool AddMember(AppUser member)
     {
-        if(!Active)
+        if(!Active||_members.Any(m => m.Id == member.Id))
             return false;
-        throw new NotImplementedException();
+
+        _members.Add(member);
+        RecalculateLedger();
+        return true;
+    }
+    public bool RemoveMember(AppUser member)
+    {
+        if(!Active||!_members.Any(m => m.Id == member.Id))
+            return false;
+        
+        var success = _members.Remove(member);
+        if (success)
+        {
+            RecalculateLedger();
+            return success;
+        }
+        else
+            return success;
     }
     private List<Tag> GetAllTags()
     {
