0a5544acd630e1bf58be8dd64a3f12dcd76575e4
Gronsak <4255265+Gronsak@users.noreply.github.com>
2026-02-22 23:16:05 +0100

    Refactor Treasurer class to include logging and separate ledger fetching methods for reusability; implement UpdateLedgerAsync for safer updates

--- Services/Treasurer.cs
+++ Services/Treasurer.cs
@@ -6,10 +6,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Coffer.Services;
 
-public class Treasurer(UserManager<AppUser> userManager, IDbContextFactory<ApplicationDbContext> dbContextFactory) : ITreasurer
+public class Treasurer(UserManager<AppUser> userManager, IDbContextFactory<ApplicationDbContext> dbContextFactory, ILogger<Treasurer> logger) : ITreasurer
 {
     private UserManager<AppUser> UserManager { get; set; } = userManager;
     private IDbContextFactory<ApplicationDbContext> DbContextFactory { get; set; } = dbContextFactory;
+    private readonly ILogger<Treasurer> Logger = logger;
 
     public async Task<bool> CreateLedgerAsync(string name, AppUser owner, Currency defaultCurrency, string description = "", ShareType defaultType = ShareType.Shares)
     {
@@ -61,22 +62,8 @@ public class Treasurer(UserManager<AppUser> userManager, IDbContextFactory<Appli
     {
         using var db = await DbContextFactory.CreateDbContextAsync();
 
-        var ledger = await db.Ledgers
-            .Include(l => l.Costs)
-                .ThenInclude(c => c.Tags)
-            .Include(l => l.IOUs)
-            .Include(l => l.Members)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.SingleCost)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.IncludeTags)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.ExcludeTags)
-            .Include(l => l.Stakes)
-            .Where(l => l.Id == id)
-            .OrderBy(l => l.Name)
-            .FirstOrDefaultAsync();
-
+        var ledger = await FetchLedgerByIdAsync(id, db);
+        
         if (ledger is null)
             return new();
 
@@ -87,21 +74,7 @@ public class Treasurer(UserManager<AppUser> userManager, IDbContextFactory<Appli
     {
         using var db = await DbContextFactory.CreateDbContextAsync();
 
-        var ledger = await db.Ledgers
-            .Include(l => l.Costs)
-                .ThenInclude(c => c.Tags)
-            .Include(l => l.IOUs)
-            .Include(l => l.Members)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.SingleCost)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.IncludeTags)
-            .Include(l => l.Shares)
-                .ThenInclude(s => s.ExcludeTags)
-            .Include(l => l.Stakes)
-            .Where(l => l.Name == name)
-            .OrderBy(l => l.Name)
-            .FirstOrDefaultAsync();
+        var ledger = await FetchLedgerByNameAsync(name, db);
 
         return ledger;
     }
@@ -220,9 +193,185 @@ public class Treasurer(UserManager<AppUser> userManager, IDbContextFactory<Appli
 
         return await GetLedgersAsync(user);
     }
-
-    public async Task<bool> UpdateLedgerAsync(Ledger ledger)
+    //TODO: more logging!
+    public async Task<bool> UpdateLedgerAsync(Ledger updatedLedger)
     {
-        throw new NotImplementedException(); //TODO: implement
+        if(updatedLedger.Id == default)
+        {
+            Logger.LogError("Ledger was not retrived, Id was set to GUID default!");
+            return false;
+        }
+        
+        using var db = await DbContextFactory.CreateDbContextAsync();
+
+        var oldLedger = await FetchLedgerByIdAsync(updatedLedger.Id, db);
+
+        if(oldLedger is null)
+        {
+            Logger.LogError("Ledger {updatedLedger.Id} was not found!", updatedLedger.Id);
+            return false;
+        }
+
+        if(oldLedger.Name != updatedLedger.Name)
+            oldLedger.Name = updatedLedger.Name;
+
+        if(oldLedger.Description != updatedLedger.Description)
+            oldLedger.Description = updatedLedger.Description;
+        
+        if(oldLedger.Owner != updatedLedger.Owner)
+        {
+            var owner = await db.Users.FindAsync(updatedLedger.Owner.Id);
+            if(owner is null)
+            {
+                Logger.LogError("User {updatedLedger.Owner.Id} was not found when changing owner!", updatedLedger.Owner.Id);
+                return false;
+            }
+            oldLedger.Owner = owner;
+        }
+
+        try
+        {
+            oldLedger = await UpdateMembers(oldLedger, updatedLedger.Members, db);
+        }
+        catch(ArgumentException e)
+        {
+            Logger.LogError(e, "There was an exception while trying to update Members!");
+            return false;
+        }
+
+        try
+        {
+            oldLedger = await UpdateCosts(oldLedger, updatedLedger.Costs, db);
+        }
+        catch(ArgumentException e)
+        {
+            Logger.LogError(e, "There was an exception while trying to update Costs!");
+            return false;
+        }
+
+        // TODO: add logic for Shares
+        // TODO: add logic for IOUs (maybe, this should really be handled internally in the ledger and might not be needed)
+        // TODO: add logic for Stakes (maybe, this is really handled internally in the ledger and might not be needed)
+
+        try
+        {
+            await db.SaveChangesAsync();
+        }
+        catch(Exception e)
+        {
+            Logger.LogError(e, "There was an error saving ledger to the Database!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static async Task<Ledger?> FetchLedgerByIdAsync(Guid id, ApplicationDbContext context)
+    {
+        var ledger = context.Ledgers
+            .Include(l => l.Costs)
+                .ThenInclude(c => c.Tags)
+            .Include(l => l.IOUs)
+            .Include(l => l.Members)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.SingleCost)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.IncludeTags)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.ExcludeTags)
+            .Include(l => l.Stakes)
+            .Where(l => l.Id == id)
+            .OrderBy(l => l.Name)
+            .FirstOrDefault();
+        return ledger;
+    }
+    private static async Task<Ledger?> FetchLedgerByNameAsync(string name, ApplicationDbContext context)
+    {
+        var ledger = await context.Ledgers
+            .Include(l => l.Costs)
+                .ThenInclude(c => c.Tags)
+            .Include(l => l.IOUs)
+            .Include(l => l.Members)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.SingleCost)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.IncludeTags)
+            .Include(l => l.Shares)
+                .ThenInclude(s => s.ExcludeTags)
+            .Include(l => l.Stakes)
+            .Where(l => l.Name == name)
+            .OrderBy(l => l.Name)
+            .FirstOrDefaultAsync();
+        return ledger;
+    }
+    private static async Task<Ledger> UpdateMembers(Ledger ledger, IEnumerable<AppUser> members, ApplicationDbContext context)
+    {
+        if(!ledger.Members.SequenceEqual(members)){
+            foreach(var member in members)
+            {
+                // TODO: rewrite with FindAsync ?
+                if(!await context.Users.AnyAsync(u => u.Id == member.Id))
+                    throw new ArgumentException($"User with Id {member.Id} does not exist!");
+                if(!ledger.Members.Any(m => m.Id == member.Id))
+                {
+                    ledger.AddMember(await context.Users.SingleAsync(u => u.Id == member.Id));
+                }
+            }
+            var membersToRemove = ledger.Members.Where(m => !members.Any(m2 => m2.Id == m.Id));
+
+            if (membersToRemove is not null)
+                foreach(var member in membersToRemove)
+                {
+                    ledger.RemoveMember(member);
+                }
+        }
+        return ledger;
+    }
+    private static async Task<Ledger> UpdateCosts(Ledger ledger, IEnumerable<Cost> costs, ApplicationDbContext context)
+    {
+        if(!ledger.Costs.SequenceEqual(costs))
+        {
+            foreach(var cost in costs)
+            {
+                var payingUser = await context.Users.FindAsync(cost.PayedBy.Id);
+                var addingUser = await context.Users.FindAsync(cost.AddedBy.Id);
+
+                if(payingUser is null || addingUser is null)
+                    if(payingUser is null)
+                        throw new ArgumentException($"User with Id {cost.PayedBy.Id} does not exist!");
+                    if(addingUser is null)
+                        throw new ArgumentException($"User with Id {cost.AddedBy.Id} does not exist!");
+
+                cost.PayedBy = payingUser;
+                cost.AddedBy = payingUser;
+                var tags = new List<Tag>();
+                foreach(var tag in cost.Tags)
+                {
+                    var dbTag = await context.Tags.FirstOrDefaultAsync(t => t.Name == tag.Name);
+                    if(dbTag is not null)
+                        tags.Add(dbTag);
+                    else
+                        tags.Add(tag);
+                }
+                cost.Tags = tags;
+
+                if(!ledger.Costs.Any( c => c.Id == cost.Id))
+                    ledger.AddCost(cost);
+                else
+                {
+                    var dbCost = ledger.Costs.Single(c => c.Id == cost.Id);
+                    if(dbCost != cost)
+                        dbCost.UpdateCost(cost);
+                }
+            }
+            var costsToRemove = ledger.Costs.Where(c => !costs.Any(c2 => c2.Id == c.Id));
+
+            if (costsToRemove is not null)
+                foreach(var cost in costsToRemove)
+                {
+                    ledger.RemoveCost(cost);
+                }
+        }
+        return ledger;
     }
 }
