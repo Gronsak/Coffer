d8156501ca894e0dafaf9896c983d242e89a44ff
Gronsak <4255265+Gronsak@users.noreply.github.com>
2026-02-26 16:05:05 +0100

    Add Amount to database migration

---
+++ Migrations/20260226150359_AddAmount.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coffer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shares_Currencies_ShareCurrencyISONum",
+                table: "Shares");
+
+            migrationBuilder.RenameColumn(
+                name: "ShareCurrencyISONum",
+                table: "Shares",
+                newName: "CurrencyISONum");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Shares_ShareCurrencyISONum",
+                table: "Shares",
+                newName: "IX_Shares_CurrencyISONum");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Shares",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shares_Currencies_CurrencyISONum",
+                table: "Shares",
+                column: "CurrencyISONum",
+                principalTable: "Currencies",
+                principalColumn: "ISONum",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shares_Currencies_CurrencyISONum",
+                table: "Shares");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Shares");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrencyISONum",
+                table: "Shares",
+                newName: "ShareCurrencyISONum");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Shares_CurrencyISONum",
+                table: "Shares",
+                newName: "IX_Shares_ShareCurrencyISONum");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shares_Currencies_ShareCurrencyISONum",
+                table: "Shares",
+                column: "ShareCurrencyISONum",
+                principalTable: "Currencies",
+                principalColumn: "ISONum",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
